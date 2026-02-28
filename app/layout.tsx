7e95a2b0c4108ee6d739e22c66d651efcbc25c92
Glen C <glenc93@gmail.com>
2026-02-28 12:32:20 +0000

    added vercel analytics to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import { Analytics } from "@vercel/analytics/next"
 import { Inter, Nunito } from 'next/font/google'
 import './globals.css'
 
@@ -20,7 +21,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${nunito.variable} font-sans`}>{children}</body>
+      <body className={`${inter.variable} ${nunito.variable} font-sans`}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
