bb4190fc487992e22f346611285b1e3a5b177007
Glen Close <glenc93@gmail.com>
2026-09-11 21:24:01 +0100

    fix(resolveVulnerabilities): upgraded to next 16

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,10 @@ import ContentGrid from '@/components/ContentGrid'
 import TealBanner from '@/components/TealBanner'
 import Footer from '@/components/Footer'
 
+// TODO: Cache Components adoption. Refactor this route so this opt-out can be removed.
+// See: https://nextjs.org/docs/app/guides/migrating-to-cache-components
+export const instant = false;
+
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col">
